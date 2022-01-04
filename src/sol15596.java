c04f05224f5e27130c5110219f087b02a0def87b
Lee-Ha-Eun-00 <lhe543@naver.com>
2022-01-04 13:13:10 +0900

    level5

---
+++ src/sol15596.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class sol15596 {
+    public static void main(String[] args)
+    {
+
+    }
+
+    public class Test{
+        long sum(int[] a)
+        {
+            int sum =0;
+            for(int i=0; i<a.length;i++)
+            {
+                sum += a[i];
+            }
+
+            return sum;
+        }
+    }
+
+}
