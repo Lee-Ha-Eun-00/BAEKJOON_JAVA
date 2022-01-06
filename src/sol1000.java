32b0365f94763d8d2162b833dd2b3c0ed0af2701
Lee-Ha-Eun-00 <lhe543@naver.com>
2022-01-06 17:28:16 +0900

    class1 - A+B

---
+++ src/sol1000.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class sol1000 {
+    public static void main(String[] args){
+        Scanner scanner =new Scanner(System.in);
+
+        int input1 = scanner.nextInt();
+        int input2 = scanner.nextInt();
+
+        System.out.println(input1+input2);
+    }
+}
