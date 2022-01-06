60c9baf8d4abc523bbfa64df6f81b9dec5ff5804
Lee-Ha-Eun-00 <lhe543@naver.com>
2022-01-06 17:41:21 +0900

    class1 - A/B

---
+++ src/sol1008.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class sol1008 {
+    public static void main(String[] args){
+        Scanner scanner =new Scanner(System.in);
+
+        double input1 = scanner.nextInt();
+        double input2 = scanner.nextInt();
+        System.out.println(input1/input2);
+    }
+}
